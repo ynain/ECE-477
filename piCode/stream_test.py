17d4b25569ea185656f22492f550b48cc5ec9824
Ian Sibley <isibley@purdue.edu>
2019-01-26 23:03:26 +0000

    Getting to streaming potential

--- piCode/stream_test.py
+++ piCode/stream_test.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+
 import io
 import time
+import socket
 import picamera
 import picamera.array
 import cv2
 
+HOST = '128.46.96.231'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with picamera.PiCamera() as camera:
     camera.start_preview()
@@ -13,3 +18,20 @@ with picamera.PiCamera() as camera:
 
         image = stream.array
         cv2.imwrite("stream_mockup.png", cv2.cvtColor(stream.array, cv2.COLOR_BGR2RGB))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST< PORT))
+    s.listen()
+
+    print("Waiting for...")
+    conn, addr = s.accept()
+    print("connection on {}:{}".format(HOST, PORT))
+
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
