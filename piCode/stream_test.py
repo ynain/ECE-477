29c2254ca2b7a4bd2da579d6269d9264548391be
isibley <isibley@purdue.edu>
2019-01-27 00:53:01 -0500

    fps update

--- piCode/stream_test.py
+++ piCode/stream_test.py
@@ -10,28 +10,34 @@ import cv2
 HOST = '128.46.96.231'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST< PORT))
+s.listen()
+
+print("Waiting for...")
+conn, addr = s.accept()
+print("connection on {}:{}".format(HOST, PORT))
+
 with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(0.2)
+    data = []
+
+    start = time.time()
     with picamera.array.PiRGBArray(camera) as stream:
+        for _ in range(24):
         camera.capture(stream, format="bgr")
 
-        image = stream.array
-        cv2.imwrite("stream_mockup.png", cv2.cvtColor(stream.array, cv2.COLOR_BGR2RGB))
+        data.append( stream.array)
+    
+    #cv2.imwrite("stream_mockup.png", cv2.cvtColor(stream.array, cv2.COLOR_BGR2RGB))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST< PORT))
-    s.listen()
+with conn:
+    print('Connected by', addr)
+    
+    conn.sendall(data)
 
-    print("Waiting for...")
-    conn, addr = s.accept()
-    print("connection on {}:{}".format(HOST, PORT))
+finisy = time.time()
 
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+print("{} seconds for {} fps".format(finish - start, 24.0 / finish-start))
 
