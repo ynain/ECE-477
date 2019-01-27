4e5440996e063f471436cbd59dc02ba0c318145f
Ian Sibley <isibley@purdue.edu>
2019-01-27 07:59:21 +0000

    Got streaming working from example, threading may be needed for 4fps

--- piCode/stream_test.py
+++ piCode/stream_test.py
@@ -11,7 +11,7 @@ HOST = '128.46.96.231'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST< PORT))
+s.bind((HOST, PORT))
 s.listen()
 
 print("Waiting for...")
@@ -21,23 +21,19 @@ print("connection on {}:{}".format(HOST, PORT))
 with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(0.2)
-    data = []
 
     start = time.time()
-    with picamera.array.PiRGBArray(camera) as stream:
-        for _ in range(24):
-        camera.capture(stream, format="bgr")
-
-        data.append( stream.array)
+    # for _ in range(24):
+    camera.capture(s, 'png')
     
-    #cv2.imwrite("stream_mockup.png", cv2.cvtColor(stream.array, cv2.COLOR_BGR2RGB))
-
+"""
 with conn:
     print('Connected by', addr)
     
     conn.sendall(data)
-
-finisy = time.time()
+"""
+finish = time.time()
+s.close()
 
 print("{} seconds for {} fps".format(finish - start, 24.0 / finish-start))
 
