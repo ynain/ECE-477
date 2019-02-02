bd5ab3014ea9d3521ff09f03f01a63148348a9bc
Ian Sibley <isibley@purdue.edu>
2019-02-01 19:42:11 -0500

    Should have been a directory name

--- facenet_trials/teststart.py
+++ facenet_trials/teststart.py
@@ -1,5 +1,5 @@
 import facenet_recognition
-facenet_recognition.align_input('input_images','aligned_images')
+print(facenet_recognition.align_input('input_images','aligned_images'))
 
 """
     Train & Test Classifier on Images
@@ -11,7 +11,7 @@ my_class ='./myclassifier/my_classifier.pkl' #location where we want to save
 test_classifier_type = 'svm' #type of model either svm or nn
 weight = './myclassifier/model_small.yaml' #local stored weights
 
-facenet_recognition.test_train_classifier(aligned_images,pre_model,my_class,weight,test_classifier_type,nrof_train_images_per_class=30, seed=102)
+facenet_recognition.test_train_classifier('aligned_images',pre_model,my_class,weight,test_classifier_type,nrof_train_images_per_class=30, seed=102)
 
 """
     Train Classifer on Images(only Training)
@@ -23,7 +23,7 @@ my_class ='./myclassifier/my_classifier.pkl' #location where we want to save
 test_classifier_type = 'nn' #type of model either svm or nn
 weight= './myclassifier/model_small.yaml' #local stored weights
 
-facenet_recognition.create_classifier(aligned_images,pre_model,my_class,weight,test_classifier_type)
+facenet_recognition.create_classifier('aligned_images',pre_model,my_class,weight,test_classifier_type)
 
 """
     Test Classifer on Images
@@ -35,4 +35,4 @@ my_class ='./myclassifier/my_classifier.pkl' #location where we want to save
 test_classifier_type = 'nn' #type of model either svm or nn
 weight= './myclassifier/model_small.yaml' #local stored weights
 
-facenet_recognition.test_classifier(aligned_images,pre_model,my_class,weight,test_classifier_type)
+facenet_recognition.test_classifier('aligned_images',pre_model,my_class,weight,test_classifier_type)
